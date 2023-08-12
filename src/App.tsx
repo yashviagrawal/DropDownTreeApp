138dbf481e850ff4984c5f0be92b53e6776dc8f1
Yashvi Agrawal <74981992+yashviagrawal@users.noreply.github.com>
2023-08-13 01:53:07 +0530

    Feat: Drop-Down App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { DownOutlined, SearchOutlined  } from "@ant-design/icons";
+import "./App.css"; // Import your custom CSS stylesheet
 
-function App() {
+const Sidebar: React.FC = () => {
+
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(event.target.value);
+  };
+
+  const filteredItems = ["dashboard", "contact Me", "about Me"].filter(item =>
+    item.includes(searchValue.toLowerCase())
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="sidebar">
+      <div className="search-bar">
+        <input
+          type="text"
+          placeholder="Search"
+          value={searchValue}
+          onChange={handleSearch}
+        />
+        <button>
+          <SearchOutlined />
+        </button>
+      </div>
+      {filteredItems.map(item => (
+        <a key={item} href={`#${item}`}>
+          {item.charAt(0).toUpperCase() + item.slice(1)}
         </a>
-      </header>
+      ))}
+
+      <div className="dropdown">
+        <button className="dropbtn">
+          Dropdown <DownOutlined />
+        </button>
+        <div className="dropdown-content">
+          <div className="sub-dropdown">
+            <button className="dropbtn">
+              Module 1 <DownOutlined />
+            </button>
+            <div className="sub-dropdown-content">
+            <div className="sub1-dropdown">
+            <button className="dropbtn">
+              Sub-Module 1 <DownOutlined />
+            </button>
+            <div className="sub2-dropdown-content">
+            <div className="sub2-dropdown">
+            <button className="dropbtn">
+              Form-Group 1 <DownOutlined />
+            </button>
+            <div className="sub3-dropdown-content">
+            <a href="#form1">Form 1</a>
+            <a href="#form2">Form 2</a>
+            <a href="#form3">Form 3</a>
+            <a href="#form4">Form 4</a>
+            <a href="#form5">Form 5</a>
+            </div>
+            </div>
+            </div>
+          </div>
+        </div>
+        </div>
+      </div>
+    </div>
     </div>
   );
-}
+};
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Sidebar />
+      {/* Main content */}
+    </div>
+  );
+};
 
 export default App;
